8a7ee5b515982d675efa9bd32fd7af580890c55b
makotosvetlapali <donpedjas@gmail.com>
2019-10-07 14:59:53 +0200

    Finished

--- src/exercises/_1_SpiceWorld.java
+++ src/exercises/_1_SpiceWorld.java
@@ -1,19 +1,11 @@
 package exercises;
 
 public class _1_SpiceWorld {
-	/*
-	 * Replace the anonymous inner class with a lambda expression.
-	 */
 
-	public static void main(String[] args) {
-		SpiceGirl sporty = new SpiceGirl() {
-			
-			@Override
-			public void perform() {
-				System.out.println("Do backflips.");
-				
-			}
-		};
+	public static void main(String[] args) {	
+   
+		SpiceGirl sporty = () -> System.out.println("Do backflips.");
+		
 		sporty.perform();
 	}
 }
